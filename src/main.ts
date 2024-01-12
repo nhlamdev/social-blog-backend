e0873c2a3405e03ad4d124e56b2809eb15a7013c
hoang lam <lam.nh@oryza.vn>
2024-01-12 08:27:42 +0700

    change pipe config

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,12 @@ async function bootstrap() {
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
 
   dotenv.config();
 
